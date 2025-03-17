b3aa0dd81ed21a681f63fa29e27b5f9830dc02ba
Amirhossein Nouranizadeh <anouranizadeh@interrodata.com>
2025-03-17 20:50:59 +0330

    require numpy functions added.

--- metta_ul/numme.py
+++ metta_ul/numme.py
@@ -135,6 +135,22 @@ def numme_atoms():
             unwrap=False,
         )
     )
+    nmLogDet = G(PatternOperation("np.linalg.slogabsdet", wrapnpop(lambda x: np.linalg.slogdet(x).logabsdet), unwrap=False))
+    nmInv = G(PatternOperation("np.linalg.inv", wrapnpop(np.linalg.inv), unwrap=False))
+    nmEinsum = G(PatternOperation("np.einsum", wrapnpop(np.einsum), unwrap=False))
+    nmExp = G(PatternOperation("np.exp", wrapnpop(np.exp), unwrap=False))
+    nmLog = G(PatternOperation("np.log", wrapnpop(np.log), unwrap=False))
+    nmCov = G(PatternOperation("np.cov", wrapnpop(np.cov), unwrap=False))
+    nmRepeat = G(PatternOperation("np.repeat", wrapnpop(np.repeat), unwrap=False))
+    nmEye = G(PatternOperation("np.eye", wrapnpop(np.eye), unwrap=False))
+    nmOnes = G(PatternOperation("np.ones", wrapnpop(np.ones), unwrap=False))
+    nmPower = G(PatternOperation("np.power", wrapnpop(np.power), unwrap=False))
+    nmRandomRand = G(PatternOperation("np.random.rand", wrapnpop(np.random.rand), unwrap=False))
+    nmDiag = G(PatternOperation("np.diag", wrapnpop(np.diag), unwrap=False))
+    nmSqrt = G(PatternOperation("np.sqrt", wrapnpop(np.sqrt), unwrap=False))
+    nmArgsort = G(PatternOperation("np.argsort", wrapnpop(np.argsort), unwrap=False))
+    nmArange = G(PatternOperation("np.arange", wrapnpop(np.arange), unwrap=False))
+    nmTake = G(PatternOperation("np.take", wrapnpop(np.take), unwrap=False))
 
     return {
         "np.vector": nmVectorAtom,
@@ -151,4 +167,20 @@ def numme_atoms():
         "np.one_hot": nmOneHot,
         "np.expand_dims": nmExpandDims,
         "np.choose": nmChoose,
+        "np.linalg.slogabsdet": nmLogDet,
+        "np.linalg.inv": nmInv,
+        "np.einsum": nmEinsum,
+        "np.exp": nmExp,
+        "np.log": nmLog,
+        "np.cov": nmCov,
+        "np.repeat": nmRepeat,
+        "np.eye": nmEye,
+        "np.ones": nmOnes,
+        "np.power": nmPower,
+        "np.random.rand": nmRandomRand,
+        "np.diag": nmDiag,
+        "np.sqrt": nmSqrt,
+        "np.argsort": nmArgsort,
+        "np.arange": nmArange,
+        "np.take": nmTake
     }
